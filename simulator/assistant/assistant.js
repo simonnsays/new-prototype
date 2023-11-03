67b42557a663bdce90dd25f088503e4f15ed582b
John Darel De Chavez <johndarel0914@gmail.com>
2023-11-03 18:45:42 +0800

    added assistant modal(for game instructions)

--- simulator/assistant/assistant.js
+++ simulator/assistant/assistant.js
@@ -3,10 +3,9 @@ const imageContainer = document.querySelector('.assistant-image-container')
 const pulse = document.querySelector('#pulse')
 const image = document.querySelector('.assistant-image')
 const infoContainer = document.querySelector('.assistant-info-container')
+const asstModal = document.querySelector('.assistant-modal')
 
 let notifCount = 1
-
-
 
 window.addEventListener('mousemove', (e) => {
     if (notifCount > 0) {
@@ -23,9 +22,18 @@ window.addEventListener('mousemove', (e) => {
         container.addEventListener('transitionend', () => {
         })
     } else {
-        notifCount = 1
-        console.log('false')
+        notifCount = 0
         container.classList.remove('extended')
+    }
+})
+
+container.addEventListener('click', () => {
+    asstModal.showModal()
+})
+
+window.addEventListener('click', (e) => {
+    if(!isInArea(container, e)) {
+        asstModal.close()
     }
 })
 
