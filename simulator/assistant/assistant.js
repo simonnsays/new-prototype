54219eafcc85e9345247b9d3d1ca1601e2fc1543
John Darel De Chavez <johndarel0914@gmail.com>
2023-11-06 09:41:01 +0800

    fixed interactions

--- simulator/assistant/assistant.js
+++ simulator/assistant/assistant.js
@@ -37,9 +37,16 @@ class Assistant {
         if(this.isInArea(this.container, e)) { 
             this.notifCount = 0
             this.container.classList.add('extended')
+            this.container.addEventListener('transitionend', () => {
+                setTimeout(() => {
+                    this.infoContainer.classList.remove('hidden');
+                }, 0);
+            })
+            
         } else {
             this.notifCount = 1
             this.container.classList.remove('extended')
+            this.infoContainer.classList.add('hidden')
         }
     }
 
@@ -63,58 +70,5 @@ class Assistant {
     }
 }
 
-
-
-// const container = document.querySelector('.assistant-container')
-// const imageContainer = document.querySelector('.assistant-image-container')
-// const pulse = document.querySelector('#pulse')
-// const image = document.querySelector('.assistant-image')
-// const infoContainer = document.querySelector('.assistant-info-container')
-// const asstModal = document.querySelector('.assistant-modal')
-
-// let notifCount = 1
-
-// window.addEventListener('mousemove', (e) => {
-//     if (notifCount > 0) {
-//         pulse.classList.add('pulse')
-//         image.classList.add('img-rltv')
-//     } else {
-//         pulse.classList.remove('pulse')
-//         image.classList.remove('img-rltv')
-//     }
-
-//     if(isInArea(container, e)) {
-//         notifCount = 0
-//         container.classList.add('extended')
-//         container.addEventListener('transitionend', () => {
-//         })
-//     } else {
-//         notifCount = 1
-//         container.classList.remove('extended')
-//     }
-// })
-
-// container.addEventListener('click', () => {
-//     asstModal.showModal()
-// })
-
-// window.addEventListener('click', (e) => {
-//     if(!isInArea(container, e)) {
-//         asstModal.close()
-//     }
-// })
-
-// function isInArea(area, mouse) {
-//     point = {x: mouse.clientX, y: mouse.clientY}
-//     rect = area.getBoundingClientRect()
-
-//     if(mouse.x > rect.x &&
-//         mouse.x < rect.x + rect.width &&
-//         mouse.y > rect.y &&
-//         mouse.y < rect.y + rect.height) {
-//             return true
-//         }
-//     return false
-// }
 
 export default Assistant
